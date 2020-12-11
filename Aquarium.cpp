13429a36dd008f9f9fa20192e66c182fc135a4c0
yac1n3 <amanouneycine@gmail.com>
2020-12-11 17:18:53 +0100

    Aquarium

--- Aquarium.cpp
+++ Aquarium.cpp
@@ -1,18 +1,15 @@
 #include "Aquarium.h"
 
-#include "Milieu.h"
+#include "Environnement.h"
 
 
 Aquarium::Aquarium( int width, int height, int _delay ) : CImgDisplay(), delay( _delay )
 {
 
-   int         screenWidth = 1280; //screen_width();
-   int         screenHeight = 1024; //screen_height();
-
 
    cout << "const Aquarium" << endl;
 
-   flotte = new Milieu( width, height );
+   flotte = new Environnement();
    assign( *flotte, "Simulation d'ecosysteme" );
 
    move( static_cast<int>((screenWidth-width)/2), static_cast<int>((screenHeight-height)/2) );
@@ -29,6 +26,22 @@ Aquarium::~Aquarium( void )
 
 }
 
+void Aquarium::Birth()
+{
+   flotte->AddCritter();
+}
+
+void Aquarium::Kill(int id)
+{
+   delete flotte->GetCritterById(id);
+}
+
+void Aquarium::SetBehavior(int id, String strategy)
+{
+   flotte->GetCritterById->ChangeBehaviour(strategy);
+}
+
+
 
 void Aquarium::run( void )
 {
@@ -40,14 +53,36 @@ void Aquarium::run( void )
 
       // cout << "iteration de la simulation" << endl;
 
-      if ( is_key() ) {
-         cout << "Vous avez presse la touche " << static_cast<unsigned char>( key() );
-         cout << " (" << key() << ")" << endl;
-         if ( is_keyESC() ) close();
+      if (is_keyESC()){
+         close();
       }
 
-      flotte->step();
-      display( *flotte );
+      if (is_key(cimg::keyB)){
+         this->Birth(); 
+
+      }
+
+      if (is_key(cimg::keyK)){
+         int critter;
+         cout << "Quelle créature voulez vous tuer ?"<<endl;
+         cin>>critter;
+         this->Kill(critter);
+      }
+
+      if (is_key(cimg::keyS)){
+         int critter;
+         String behavior;
+         cout << "De quelle créature voulez-vous modifier le comportement ?"<<endl;
+         cin>>critter;
+         cout << "Quel comportement voulez-vous lui attribuer ?"<<endl;
+         cin<<behavior;
+         this->setBehavior(critter,behavior);
+      }
+
+
+
+      flotte->UpdateCritters();
+      Draw( *flotte );
 
       wait( delay );
 
