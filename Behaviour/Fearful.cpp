6da2f88c6d9d946234d96d754cbe8be1fc5b2204
Xi SONG <sungxisx@icloud.com>
2020-12-15 09:40:09 +0800

    Add more comments for behavior

--- Behaviour/Fearful.cpp
+++ Behaviour/Fearful.cpp
@@ -11,12 +11,12 @@ Fearful::Fearful(){}
 Fearful::~Fearful(){}
 
 float * Fearful::NextMove(CritterInterface* critter, std::vector<std::shared_ptr<CritterInterface>> listcritters) {
-    std::vector<std::shared_ptr<CritterInterface>> critters = critter->Detect(listcritters);
-    int number = critters.size();
+    std::vector<std::shared_ptr<CritterInterface>> detected_critters = critter->Detect(listcritters);
+    int number = detected_critters.size();
     const float* direction = critter->GetDirection();
     const float* position = critter->GetPosition();
-    float dir[2] = {0, 0};
-//    To the opposite direction
+    float * dir =  new float[2];
+    // To the opposite direction
     if (number > FEAR_THRESHOLD) {
         dir[0] = position[0] * 2 - direction[0];
         dir[1] = position[1] * 2 - direction[1];
--- Behaviour/Kamikaze.cpp
+++ Behaviour/Kamikaze.cpp
@@ -16,20 +16,18 @@ int * Kamikaze::GetColor() {
 
 float * Kamikaze::NextMove(CritterInterface *critter, std::vector<std::shared_ptr<CritterInterface>> listcritters) {
     std::vector<std::shared_ptr<CritterInterface>> critters = critter->Detect(listcritters);
-    float nearest_critter[2] = {critters[0]->GetDirection()[0], critters[0]->GetDirection()[1]};
-    float current_position[2] = {};
-    current_position[0] = critter->GetPosition()[0];
-    current_position[1] = critter->GetPosition()[1];
-    float *result =  {0, 0};
+    float *result =  new float[2];
     int num = critters.size();
-    if(num > 0) {
+    if(num > 0){
+        const float * nearest_critter = critters.at(0)->GetDirection();
+        const float * current_position = critter->GetPosition();
         float distance = getDistance(current_position, nearest_critter);
         float temporary_distance;
-   
+
+//        Choose the nearest critter
         for (int i = 1; i < num; ++i) {
             float temporary_position[2] = {critters[i]->GetPosition()[0], critters[i]->GetPosition()[1]};
             temporary_distance = getDistance(current_position, temporary_position);
-            // Choose the nearest critter
             if (temporary_distance > distance) {
                 result[0] = temporary_position[0];
                 result[1] = temporary_position[1];
@@ -41,7 +39,6 @@ float * Kamikaze::NextMove(CritterInterface *critter, std::vector<std::shared_pt
     return result;
 }
 
-// Calculate the distance between two coordinate.
 float Kamikaze::getDistance(const float * p1, const float * p2) {
     return pow(pow(p1[0] - p2[0], 2) + pow(p1[1] - p2[1], 2), 0.5);
 }
