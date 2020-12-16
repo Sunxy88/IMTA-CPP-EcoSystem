cc84cbddf4c0ff59abf235870224189e7379a936
Paul Beslin <paul.beslin@gmail.com>
2020-12-16 16:17:19 +0100

    Corrected merge issues

--- BaseCritter.cpp
+++ BaseCritter.cpp
@@ -177,11 +177,7 @@ const int BaseCritter::GetLifespan() const {return this->lifespan; }
 
 const int BaseCritter::GetCurrentAge() const {return this->age; }
 
-<<<<<<< HEAD
 BehaviourInterface* BaseCritter::GetBehaviour() {return this->behaviour; }
-=======
-BehaviourInterface* BaseCritter::GetBehaviour() {return this->behaviour;}
->>>>>>> 4107fd477036fc90861f7d0e67ceca1a07f3d375
 
 const bool BaseCritter::GetMultiBehaviour() const {return this->isMultiBehaviour; }
 
@@ -189,32 +185,15 @@ void BaseCritter::setIsDying(bool dead){this->isDead = dead;}
 
 
 void BaseCritter::Draw(UImg & support){
-<<<<<<< HEAD
-
 	const float orientation = atan(this->direction[1] / this->direction[0]) * 180 / M_PI;
 	const float maxSize = std::max(this->size[0], this->size[1]);
-=======
-	//const float HEADRATIO = 2.1;
-
-	const float orientation = atan(this->direction[1] / this->direction[0]) * 180 / M_PI;
-	/*const float maxSize = std::max(this->size[0], this->size[1]);
-	const double xt = this->position[0] + direction[0]*maxSize/HEADRATIO;
-	const double yt = this->position[1] - direction[1]*maxSize/HEADRATIO;*/
->>>>>>> 4107fd477036fc90861f7d0e67ceca1a07f3d375
-    
 	
 	//std::cout << this->behaviour->GetColor()[0] << std::endl;
 	support.draw_ellipse(this->position[0], this->position[1], this->size[0], this->size[1], orientation, this->behaviour->GetColor());
-<<<<<<< HEAD
 	const float black[3] = {0, 0, 0};
 	support.draw_text(this->position[0], this->position[1], std::to_string(this->id).c_str(), black, 1);
 	const float norm = std::pow( std::pow(this->direction[0], 2) + std::pow(this->direction[1], 2), 0.5 ) * 0.5/maxSize;
 	support.draw_arrow(this->position[0], this->position[1], this->position[0] + this->direction[0] / norm, this->position[1] + this->direction[1] / norm, black);
-=======
-	int text_color[3] = {100,100,0};
-	support.draw_text(this->position[0], this->position[1], std::to_string(GetId()).c_str(), text_color, 1, 1.0, 22);
-	//support.draw_circle( xt, yt, maxSize/HEADRATIO, this->behaviour->GetColor());
->>>>>>> 4107fd477036fc90861f7d0e67ceca1a07f3d375
 }
 
 void BaseCritter::MoveTowards(const float newDirection[DIM]){
